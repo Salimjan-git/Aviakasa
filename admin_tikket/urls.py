2f8e424413d0d49243ac65f72d5d8b85d262d2c2
shahriyor <islomovrofe@gmail.com>
2025-11-25 17:54:30 +0500

    last_coomit_flight

--- admin_tikket/urls.py
+++ admin_tikket/urls.py
@@ -25,6 +25,7 @@ from .views import (
 
 
 urlpatterns = [
+    
     path("", home, name='home'),
     path("register/", register_view, name='register'),
     path("login/", login_view, name='login'),
